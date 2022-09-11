bf74b1a07e2dd49363eeaae8785be4fa0ceff9de
Bioms-Louis <louisbioms20@gmail.com>
2022-09-11 08:42:42 +0100

    Update index.php

--- index.php
+++ index.php
@@ -93,23 +93,23 @@ include "include/menu.php";
 	<div class="row">
 		<div class="column">
 			<h3>SICK BAY</h3> 
-			<img src="" alt="">
+			<img src="https://www.edocns.edu.ng/wp-content/uploads/2013/12/Sick-Bay.jpg" alt="">
 			<p style="color:white; font-size: 13px; padding: 20px">
 			Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
             aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 			</p>
 		</div>
 		<div class="column">
-			<h3>CLASS ROOMS</h3>
-			<img src="" alt="" style="background-size: contain;"> 
+			<h3>STUDENT DEMONSTARTION ROOMS</h3>
+			<img src="https://www.edocns.edu.ng/wp-content/uploads/2013/04/Demonstration.jpg" alt="" style="background-size: contain;"> 
 			<p style="color:white; font-size: 13px; padding: 20px">
 			Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
             aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 			</p>
 		</div>
 		<div class="column">
-			<h3>CLINIC</h3>
-			<img src="" alt="">
+			<h3>WARD</h3>
+			<img src="https://www.edocns.edu.ng/wp-content/uploads/2014/01/Students-in-the-Wards.jpg" alt="">
 			<p style="color:white; font-size: 13px; padding: 20px">
 			Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
             aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
